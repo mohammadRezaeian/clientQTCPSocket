ed03d0fd64c49b3dd2ceb118f55deb6505b85200
Mohammad Rezaeian <emailwork@riseup.net>
2023-03-26 10:12:11 +0330

    read vectore recived from server

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <QCoreApplication>
 #include "createConnectionCLient.h"
 #include <QTcpSocket>
+#include <QDataStream>
+#include <QVector>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QTcpSocket *_socketClient = createConnectionCLient::get_instance("localhost" , 1234);
-
     if(_socketClient->waitForConnected(3000))
     {
         qDebug() << "Connected!";
@@ -14,13 +15,16 @@ int main(int argc, char *argv[])
         // send
         _socketClient->write("hello server");
         _socketClient->flush();
-        _socketClient->waitForBytesWritten(1000);
-        _socketClient->waitForReadyRead(3000);
 
-        qDebug() << "Reading: " << _socketClient->bytesAvailable();
+        QByteArray _receivedData = _socketClient->readAll();
+        QDataStream stream(&_receivedData, QIODevice::ReadOnly);
+        QVector<double> _valueVector;
+        stream >> _valueVector;
 
-        qDebug() << _socketClient->readAll();
-
+        for (int i = 0; i < _valueVector.size(); i++) {
+            double _vectorValues = _valueVector[i];
+            qDebug() << _vectorValues;
+        }
 //        _socketClient->close();
     }
     else
